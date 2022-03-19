29b3b3b8aeb1d4b1848d75c931e9a4704fbe837a
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-19 06:04:47 +0530

    Possible final fix

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ var app = express();
 console.log("Hello World");
 var absPath = __dirname + '/views/index.html';
 var pathCss = __dirname + '/public';
-app.use(pathCss, express.static(pathCss));
+app.use('/public', express.static(pathCss));
 app.get('/', (req,res)=> {
     res.sendFile(absPath);
 });
