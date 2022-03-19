06a8fcd7c5203d9b371a18749144c6234862f415
Aryansh Malviya <aryanshmalviya007@gmail.com>
2022-03-19 05:51:33 +0530

    Add stylesheet

--- myApp.js
+++ myApp.js
@@ -2,9 +2,12 @@ var express = require('express');
 var app = express();
 console.log("Hello World");
 var absPath = __dirname + '/views/index.html';
+var pathCss = __dirname + '/public';
 app.get('/', (req,res)=> {
     res.sendFile(absPath);
 });
+
+app.use(express.static(pathCss));
 
 
 
